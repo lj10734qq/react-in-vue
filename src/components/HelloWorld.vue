344370459e5209ad6f749b0a553cf7501225f474
maclerylin <maclerylin@tencent.com>
2022-02-25 20:15:26 +0800

    提交react in vue demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Date: 2022-02-24 19:59:24
+ * @LastEditors: maclerylin
+ * @LastEditTime: 2022-02-24 20:03:22
+-->
+<template>
+    <div class="vuera">
+        <h1>It's a react component and it wraps a vue component, see following</h1>
+        <test-react-c name="maclerylin"/>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { VuePlugin } from 'vuera'
+import TestReactC from '../react-component/TestReactC.jsx';
+
+Vue.use(VuePlugin)
+
+
+export default {
+    name: 'VueRa',
+    components: {
+        TestReactC,
+    }
+}
+</script>
+
+<style scoped>
+    .vuera {
+        margin: 0 auto;
+        text-align: left;
+    }
+</style>
